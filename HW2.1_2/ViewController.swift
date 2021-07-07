0587a1b97a35fdcf0ca6543ef097cae4b426cf5d
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-07-07 21:09:33 +1000

    Initial Commit

---
+++ HW2.1_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1_2
+//
+//  Created by Kovalyov Dmitry on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
